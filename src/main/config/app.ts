767dc8efc4f9dd28e02fd84d779519bf0f8b0ecb
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-12-06 12:58:08 -0500

    chore: cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,8 +3,11 @@ import setupSwagger from './config-swagger'
 // import { config as dotenv } from 'dotenv'
 import middlewares from './middlewares'
 import routes from './routes'
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 setupSwagger(app)
 middlewares(app)
