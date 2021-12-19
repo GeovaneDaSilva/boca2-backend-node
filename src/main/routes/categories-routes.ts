c222079eab76afabb0e36e92cdb2002e33ace7e3
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-12-19 02:43:08 -0500

    fix: category id

--- src/main/routes/categories-routes.ts
+++ src/main/routes/categories-routes.ts
@@ -5,7 +5,7 @@ import { AdaptRoute } from '../adapters/express-route-adapter'
 import { makeCreateCategoryController, makeDeleteCategoryController, makeEditCategoryController, makeGetProductsCategoryByIdController, makeListCategoriesController, makeListCategoryController, makeSelectCategoriesController } from '../factories/category'
 export default (router: Router): void => {
   router.post('/category', AuthenticationToken.veryfyRole_Admin, AdaptRoute(makeCreateCategoryController()))
-  router.get('/category/:id', AuthenticationToken.veryfyToken, AdaptRoute(makeListCategoryController()))
+  router.get('/category/:id',  AdaptRoute(makeListCategoryController()))
   router.get('/categories', AdaptRoute(makeListCategoriesController()))
   router.put('/category/:id', AuthenticationToken.veryfyRole_Admin, AdaptRoute(makeEditCategoryController()))
   router.delete('/category/:category_id', AuthenticationToken.veryfyRole_Admin, AdaptRoute(makeDeleteCategoryController()))
