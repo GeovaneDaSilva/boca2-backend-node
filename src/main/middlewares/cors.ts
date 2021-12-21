294b8802f0533ecfc055cb743901fcb03a211d2b
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-12-21 16:24:45 -0500

    chore: deploy

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.set('Access-Control-Allow-Origin', '*')
-  res.set('Access-Control-Allow-Headers', '*')
-  res.set('Access-Control-Allow-Methods', '*')
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-headers', '*')
+  res.set('access-control-allow-methods', '*')
   next()
 }
-
