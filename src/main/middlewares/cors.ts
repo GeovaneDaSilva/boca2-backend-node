263e3602cca7bf46d3a653d5630566c2c1135503
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-12-21 11:39:57 -0500

    chore: add

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -5,5 +5,12 @@ export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set('access-control-allow-headers', '*')
   res.set('access-control-allow-methods', '*')
   res.set('strict-origin-when-cross-origin', '*')
+  res.set('no-referrer', '*')
+  res.set('no-referrer-when-downgrade', '*')
+  res.set('origin-when-cross-origin', '*')
+  res.set('same-origin', '*')
+  res.set('strict-origin-when-cross-origin', '*')
+  res.set('strict-origin', '*')
+  res.set('unsafe-url', '*')
   next()
 }
