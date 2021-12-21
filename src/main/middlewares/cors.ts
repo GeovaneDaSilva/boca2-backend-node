e4fc23b801fa92a86b6d72484c7b1b61a39727c5
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-12-21 11:44:32 -0500

    chore: cores

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,16 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.set('access-control-allow-origin', '*')
-  res.set('access-control-allow-headers', '*')
-  res.set('access-control-allow-methods', '*')
-  res.set('strict-origin-when-cross-origin', '*')
-  res.set('no-referrer', '*')
-  res.set('no-referrer-when-downgrade', '*')
-  res.set('origin-when-cross-origin', '*')
-  res.set('same-origin', '*')
-  res.set('strict-origin-when-cross-origin', '*')
-  res.set('strict-origin', '*')
-  res.set('unsafe-url', '*')
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Headers', '*')
+  res.set('Access-Control-Allow-Methods', '*')
   next()
 }
