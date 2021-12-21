54da9848005cd172c45a5357b07e3a0d58de6fd3
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-12-21 11:59:51 -0500

    chore: set headers

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -5,5 +5,9 @@ export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set('Access-Control-Allow-Headers', '*')
   res.set('Access-Control-Allow-Methods', '*')
   res.set('Strict-Origin-When-Cross-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
   next()
 }
