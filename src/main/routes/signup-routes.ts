913807211ddd3eb5679058aa44ee6e2994ec3dba
GeovaneDaSilva <geovaneartedesign@gmail.com>
2021-12-22 09:51:32 -0500

    chore: midd

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -7,8 +7,8 @@ export default (router: Router): void => {
   router.post('/login', AdaptRoute(makeLoginController()))
   router.post('/signup', AdaptRoute(makeSignUpController()))
   router.put('/account/:id', AdaptRoute(makeUpdateAccountController()))
-  router.get('/account/activated/:token', AuthenticationToken.veryfyToken, AdaptRoute(makeActivatedAccountController())) // confirm account
-  router.get('/accounts/activated/:value', AuthenticationToken.veryfyToken, AdaptRoute(makeGetAccountsActivatedsController())) // get accounts
+  router.get('/account/activated/:token', AdaptRoute(makeActivatedAccountController())) // confirm account
+  router.get('/accounts/activated/:value', AdaptRoute(makeGetAccountsActivatedsController())) // get accounts
 
   router.get('/accounts',  AdaptRoute(makeGetAccountsController()))
   router.get('/get_account/:id/', AdaptRoute(makeGetAccountController()))
