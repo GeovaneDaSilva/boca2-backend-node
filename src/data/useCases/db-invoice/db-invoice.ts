357800f6f6e9af9fe5e2b2425ed0492762b0ef0b
GeovaneDaSilva <geovaneartedesign@gmail.com>
2022-01-13 13:57:19 -0500

    chore: add text invoice ingles

--- src/data/useCases/db-invoice/db-invoice.ts
+++ src/data/useCases/db-invoice/db-invoice.ts
@@ -409,7 +409,54 @@ export class DbInvoice implements IInvoiceUseCase {
               </tr>
 
             <!-- END MAIN CONTENT AREA -->
+            
             </table>
+
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <table role="presentation" class="main">
+
+              <!-- START MAIN CONTENT AREA ENGLISH -->
+              <tr>
+                <td class="wrapper">
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                      <td>
+                      <p><b>Invoice: #001${insertInvoiceDb.order_number}</b></p>
+                        <p>Hi, ${insertInvoiceDb.customer_name},</p>
+                        <p>Your order was received. Please click on the link to view your invoice and complete the payment. Once the payment is processed, your order will be confirmed.</p>
+                        <p>You can make your payment by ATH Móvil to our business account /BOCApr or with credit card through this email. We accept Amex, Visa and Mastercard.  </p>
+
+                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                          <tbody>
+                            <tr>
+                              <td align="left">
+                                <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                                  <tbody>
+                                    <tr>
+                                      <td> <a href="${process.env.baseUrl}/invoice/client/${insertInvoiceDb._id}" target="_blank">Ver Factura</a> </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <p>Thanks for your support. </p>
+                        <p>If you have any questions, reply to this email or contact us at boca2foodspr@gmail.com</p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+            <!-- END MAIN CONTENT AREA -->
+            
+            </table>
+
+
+
+
             <!-- END CENTERED WHITE CONTAINER -->
 
             <!-- START FOOTER -->
